9312b4f66ccd050e4561fc4a52e23e130b9ee41e
Andrei VOICU <andrei.voicu@nstel.fr>
2020-01-15 11:14:53 +0100

    fini

--- TP3/exo7.py
+++ TP3/exo7.py
@@ -1,6 +1,5 @@
 #coding: utf-8
 
-
 def facto(x):
     if x == 1:
         return x
@@ -11,7 +10,6 @@ def expo(x):
     for i in range(2, 40):
         result += (x**i)/facto(i)
     return result
-
 
 x = float(input())
 
